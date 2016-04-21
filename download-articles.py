c48862093d553cf4a48ec490b5fb0ebcbac43fe9
Avinash Ramu <trackavinash@gmail.com>
2016-04-20 20:50:16 -0500

    encode before writing

--- download-articles.py
+++ download-articles.py
@@ -14,30 +14,38 @@ URL_REGEX = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-
 def create_date(year, month, day):
     return year + "-" + month + "-" + day
 
+n_days_per_year = 2
+n_days = 0
 years = ["2015"]
+seen_dates = {}
 for year in years:
-    month = str(randint(1, 12))
-    day = str(randint(1, 28))
-    date = create_date(year, month, day)
-    out_file = date + ".txt"
-    f_out = open(out_file, "w")
-    print("date is " + date)
-    url = "http://www.thehindu.com/todays-paper/tp-index/?date=" + date
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text,"lxml")
-    tpaper = soup.find_all("div", class_="tpaper")
-    print(tpaper)
-    tpaper_links = re.findall(URL_REGEX,str(tpaper))
-    for count, tpaper_link in tpaper_links:
-        if count == 100:
-            break
-        print(count)
-        print(tpaper_link)
-        article = newspaper.Article(tpaper_link)
-        article.download()
-        article.parse()
-        print(article.text)
-        f_out.write(str(article.text))
+    while n_days < n_days_per_year:
+        month = str(randint(1, 12))
+        day = str(randint(1, 28))
+        date = create_date(year, month, day)
+        if date in seen_dates:
+            next
+        n_days += 1
+        seen_dates[date] = 1
+        out_file = date + ".txt"
+        f_out = open(out_file, "w")
+        print("date is " + date)
+        url = "http://www.thehindu.com/todays-paper/tp-index/?date=" + date
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text,"lxml")
+        tpaper = soup.find_all("div", class_="tpaper")
+        tpaper_links = re.findall(URL_REGEX,str(tpaper))
+        for count, tpaper_link in enumerate(tpaper_links):
+            if count == 100:
+                break
+            print(count)
+            print(tpaper_link)
+            article = newspaper.Article(tpaper_link)
+            article.download()
+            article.parse()
+            print(article.text)
+            text = article.text.replace("\n", "")
+            f_out.write(text.encode('utf-8'))
 
 """
 user_agent = "DUC newspaper analysis 1.0"
