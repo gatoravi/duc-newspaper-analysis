455341156f267d6a8035cd3f8a4858c5e7d547ff
Avinash Ramu <trackavinash@gmail.com>
2016-04-21 09:08:07 -0500

    Format better

--- download-articles.py
+++ download-articles.py
@@ -32,33 +32,16 @@ for year in years:
         print("date is " + date)
         url = "http://www.thehindu.com/todays-paper/tp-index/?date=" + date
         r = requests.get(url)
-        soup = BeautifulSoup(r.text,"lxml")
+        soup = BeautifulSoup(r.text, "lxml")
         tpaper = soup.find_all("div", class_="tpaper")
         tpaper_links = re.findall(URL_REGEX,str(tpaper))
         for count, tpaper_link in enumerate(tpaper_links):
+            print "Article number: " + str(count)
             if count == 100:
                 break
-            print(count)
-            print(tpaper_link)
             article = newspaper.Article(tpaper_link)
             article.download()
             article.parse()
-            print(article.text)
-            text = article.text.replace("\n", "")
+            text = re.sub("\n", " ", article.text)
+            text = text + "\n"
             f_out.write(text.encode('utf-8'))
-
-"""
-user_agent = "DUC newspaper analysis 1.0"
-r = praw.Reddit(user_agent=user_agent)
-domain = r.get_domain_listing('thehindu.com', sort='new', limit=1000)
-urls = [x.url for x in domain]
-print("\nGot {:d} urls\n".format(len(urls)))
-
-print ("Fetching url: {}\n".format(urls[0]))
-article = newspaper.Article(urls[0])
-article.download()
-article.parse()
-
-print ("Here's the raw text:\n")
-print(article.text)
-"""
